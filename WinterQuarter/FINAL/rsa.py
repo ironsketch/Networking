0c34357b58e61237c6aeff406520fb2dabbc7e53
nullface <nullfac3@gmail.com>
2018-03-15 14:25:42 -0700

    final

--- WinterQuarter/FINAL/rsa.py
+++ WinterQuarter/FINAL/rsa.py
@@ -1,9 +1,25 @@
-p = 5
-q = 7
+import random, math
+lowerBound = input("\nEnter a lower bound (maybe 500 for faster) ")
+largePrime = input("Enter an upper bound (maybe 1000 for faster) ")
+
+def isPrime(num):
+    if num == 2:
+        return True
+    for i in range(2, num - 1):
+        if num % i == 0:
+            return False
+    return True
+
+primes = [i for i in range(lowerBound,largePrime) if isPrime(i)]
+p = random.choice(primes)
+q = random.choice(primes)
 n = p * q
 z = (p - 1) * (q - 1)
-e = 31
-d = 1
+e = random.choice(primes)
+d = 0
+
+while(e >= z):
+    e = random.choice(primes)
 
 for i in range (2, n):
     if ((e * i) % z == 1):
@@ -13,6 +29,8 @@ m = 12
 c = (m ** e) % n
 message = (c ** d) % n
 
+print("\n")
 print("Original Message: ", m)
 print("Encrypted Message: ", c)
 print("Decrypted Message: ", message)
+print("\n")
